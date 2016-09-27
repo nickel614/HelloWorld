51726c154cd7de074d7ab0f2a276a664c18cc602
nickel614 <nickel614@mail.ru>
2016-09-27 13:30:28 +0500

    Second commit

--- HelloWorld_InGit/src/Solution.java
+++ HelloWorld_InGit/src/Solution.java
@@ -10,6 +10,8 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Git!");
+        System.out.println("This is the second commit for Git");
+
     }
 
 }
