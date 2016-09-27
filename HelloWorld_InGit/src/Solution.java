6d474706261454f4f55caa0f61c21c585e559384
nickel614 <nickel614@mail.ru>
2016-09-27 12:53:39 +0500

    First HelloWorld

---
+++ HelloWorld_InGit/src/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * HelloWorld for gitHub. This is my first program in repository.
+ * Created by Nickel614 on 27.09.2016.
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Git!");
+    }
+
+}
